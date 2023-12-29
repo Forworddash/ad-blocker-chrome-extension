5f74c74b9c9615b790032180095da9615a618e0f
samuel <samuel.baker@rack-a-tiers.com>
2023-12-28 20:36:22 -0800

    - [UPD] updated list of urls to block

--- background.js
+++ background.js
@@ -1,6 +1,21 @@
+// array of urls to block
+const Filters = [
+    "*://*.doubleclick.net/*",
+    "*://partner.googleadservices.com/*",
+    "*://*.googlesyndication.com/*",
+    "*://*.google-analytics.com/*",
+    "*://creative.ak.fbcdn.net/*",
+    "*://*.adbrite.com/*",
+    "*://*.exponential.com/*",
+    "*://*.quantserve.com/*",
+    "*://*.scorecardresearch.com/*",
+    "*://*.zedo.com/*",
+    "*://*.doubleclick.com/*",
+]
+
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
         return {cancel: true}},
-        {urls: ["*://*.zedo.com/*"]},
+        {urls: Filters},
         ["blocking"]
     )
