fa894787b2cb1f74413e7ffc6a914eb451686c85
samuel <samuel.baker@rack-a-tiers.com>
2023-12-31 10:56:32 -0800

    - [FIX] check

--- background.js
+++ background.js
@@ -1,26 +1,43 @@
 // array of URLs to block
-const Filters = [
-    "*://*.doubleclick.net/*",
-    "*://partner.googleadservices.com/*",
-    "*://*.googlesyndication.com/*",
-    "*://*.google-analytics.com/*",
-    "*://creative.ak.fbcdn.net/*",
-    "*://*.adbrite.com/*",
-    "*://*.exponential.com/*",
-    "*://*.quantserve.com/*",
-    "*://*.scorecardresearch.com/*",
-    "*://*.zedo.com/*",
-    "*://*.doubleclick.com/*",
-    "*://*.googleadservices.com/*",
-    "*://*youtube.com/*",
-    "*://*.ytimg.com/*",
-];
+// const Filters = [
+//     "*://*.doubleclick.net/*",
+//     "*://partner.googleadservices.com/*",
+//     "*://*.googlesyndication.com/*",
+//     "*://*.google-analytics.com/*",
+//     "*://creative.ak.fbcdn.net/*",
+//     "*://*.adbrite.com/*",
+//     "*://*.exponential.com/*",
+//     "*://*.quantserve.com/*",
+//     "*://*.scorecardresearch.com/*",
+//     "*://*.zedo.com/*",
+//     "*://*.doubleclick.com/*",
+//     "*://*.googleadservices.com/*",
+//     "*://*youtube.com/*",
+//     "*://*.ytimg.com/*",
+// ];
 
+
+// chrome.webRequest.onBeforeRequest.addListener(
+//     function(details) {
+//         return { cancel: true };
+//     },
+//     { urls: Filters },
+//     ["blocking"]
+// );
+
+
+// create a filter to block youtube completely
+const youtubeFilter = {
+    urls: [
+        "*://*.youtube.com/*",
+        "*://*.ytimg.com/*",
+    ]
+};
 
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
         return { cancel: true };
     },
-    { urls: Filters },
+    youtubeFilter,
     ["blocking"]
 );
